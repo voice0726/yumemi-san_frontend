b8dedf218edc2a3853fe2aaa1122654d8509585c
Akinori Ito <voice0726@gmail.com>
2025-04-02 21:43:02 +0900

    chore: install tseslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +12,21 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const languageOptions = {
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+    parserOptions: {
+      project: ['./tsconfig.json'],
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+};
 
-export default eslintConfig;
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  languageOptions,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+);
