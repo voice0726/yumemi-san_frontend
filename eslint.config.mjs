959840131674150e02b98e65573ed72635bf3be7
Akinori Ito <voice0726@gmail.com>
2025-04-02 22:52:31 +0900

    test: introduce vitest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
+import vitestPlugin from '@vitest/eslint-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,9 +17,10 @@ const languageOptions = {
   languageOptions: {
     globals: {
       ...globals.node,
+      ...globals.vitest,
     },
     parserOptions: {
-      project: ['./tsconfig.json'],
+      project: ['./tsconfig.json', './tsconfig.test.json'],
       tsconfigRootDir: import.meta.dirname,
     },
   },
@@ -28,5 +30,6 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   languageOptions,
+  vitestPlugin.configs.recommended,
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
 );
